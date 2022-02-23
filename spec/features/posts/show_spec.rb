3ee4314c576c8b0f93a52b198d3f8ec91e2138a8
Josh Klina <joshua.klina@gmail.com>
2022-02-23 09:55:51 -0500

    Add tests for viewing a post

---
+++ spec/features/posts/show_spec.rb
@@ -0,0 +1,36 @@
+require "rails_helper"
+
+RSpec.feature "Viewing a post", :type => :feature do
+  scenario "a visitor can view published posts" do
+    post = create(
+      :post,
+      published: true,
+      title: "My Title",
+      body: "Some content",
+    )
+
+    visit hyper_kitten_meow_path
+    click_link("My Title")
+
+    expect(current_path).to eq(hyper_kitten_meow.post_path(post))
+    expect(page).to have_text("My Title")
+    expect(page).to have_text("Some content")
+  end
+
+  scenario "a visitor cannot view unpublished posts" do
+    post = create(:post, published: false)
+
+    expect { visit hyper_kitten_meow.post_path(post) }.
+      to raise_error(ActiveRecord::RecordNotFound)
+  end
+
+  scenario "a visitor will see the post's title as the page's title" do
+    post = create(:post, published: true, title: 'my title!', summary: 'summary')
+
+    visit hyper_kitten_meow.post_path(post)
+
+    expect(page).to have_title('my title!')
+    expect(page).to have_css("meta[name='description'][content='summary']",
+                             visible: false)
+  end
+end
