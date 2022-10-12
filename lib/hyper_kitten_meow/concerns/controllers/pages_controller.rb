0b9a058a5d83090b0921d11a7df6a6b744516617
Joshua Klina <joshua.klina@gmail.com>
2022-10-12 10:43:12 -0400

    Can use hardcoded pages in addition to CMS pages

--- lib/hyper_kitten_meow/concerns/controllers/pages_controller.rb
+++ lib/hyper_kitten_meow/concerns/controllers/pages_controller.rb
@@ -9,7 +9,17 @@ module HyperKittenMeow
         end
 
         def show
-          @page = HyperKittenMeow::Page.find_by_slug(params[:id])
+          slug = params[:id]
+          template_name = slug.underscore
+          if lookup_context.exists?(template_name, "hyper_kitten_meow/pages")
+            template = lookup_context.find(template_name, "hyper_kitten_meow/pages")
+            render template: template
+          else
+            @page = HyperKittenMeow::Page.find_by_slug(slug)
+            unless @page
+              raise ActionController::RoutingError.new("Not Found")
+            end
+          end
         end
       end
     end
---
+++ spec/features/pages_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+
+RSpec.feature "Displaying pages", :type => :feature do
+  scenario "visitor can view hardcoded pages" do
+    visit hyper_kitten_meow.page_path("hardcoded-page")
+
+    expect(page).to have_text("This is a hardcoded page")
+  end
+
+  scenario "visitor can view pages created in the database" do
+    cms_page = create(:page, slug: "test-page", title: "This is a test page")
+
+    visit hyper_kitten_meow.page_path("test-page")
+
+    expect(page).to have_text("This is a test page")
+  end
+end
