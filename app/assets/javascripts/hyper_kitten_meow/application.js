06578c70ed1e631f967bf6b3e4369d7eb9b5b15c
Joshua Klina <joshua.klina@gmail.com>
2022-10-13 16:15:40 -0400

    Fix popper

--- app/assets/javascripts/hyper_kitten_meow/application.js
+++ app/assets/javascripts/hyper_kitten_meow/application.js
@@ -1,9 +1,11 @@
 import "@hotwired/stimulus"
-import "@popperjs/core"
 import "@rails/actiontext"
 import "@rails/activestorage"
 import "trix"
 import * as bootstrap from "bootstrap"
 import "hyper_kitten_meow/controllers"
+
+// Hack to appease Popper
+window.process = { env: {} }
 
 console.log("Hello from HyperKittenMeow")
